27bdd4eb0ccf130eb872a852f3ab5309ad314392
Robert Uhl <ruhl@octopodial-chrome.com>
2013-03-31 21:57:32 -0600

    Added hash reading test

--- spki_test.go
+++ spki_test.go
@@ -3,6 +3,7 @@ package spki
 import (
 	"testing"
 	"crypto/sha256"
+	"github.com/eadmund/sexprs"
 )
 
 func TestHash(t *testing.T) {
@@ -13,5 +14,14 @@ func TestHash(t *testing.T) {
 	if !h1.Equal(h2) {
 		t.Fatal("Equal hashes are not Equal()")
 	}
+	sexp, _, err := sexprs.ReadBytes([]byte("(hash sha512 #7356f5d518d0a4f02741fee41b851cacfd428f02dc6c92557807dc6b51a97c2f3404121eb752a7c14819143a9273aff47bef5f7305e1476f4ab338832bc9d022#)"))
+	if err != nil {
+		t.Fatal("Error reading hash S-expression", err)
+	}
+	h1, err = EvalHash(sexp)
+	if err != nil {
+		t.Fatal("Error evaluation hash S-expression", err)
+	}
+	//t.Log(h1.Sexp())
 	//t.Log(h.Sexp())
 }
